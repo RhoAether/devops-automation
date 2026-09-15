96f67a5b3c348d0626c46862e72d983297b23c62
Rhoe-kun <rbornea_230000002480@uic.edu.ph>
2026-09-15 13:10:11 +0000

    Fix URL typo in hello_automation.py

--- devops-automation/hello_automation.py
+++ devops-automation/hello_automation.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-response = requests.get('htpp://localhost:8000/api/')
+response = requests.get('http://localhost:8000/api/')
 print(f"NetBox Status: {responding.status_code}")
 if response.status_code == 200: 
     print("NetBox is running successfully!")
