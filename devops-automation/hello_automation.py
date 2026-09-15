fcaf2e55de1728d26d22f41515b1ea874838b8be
Rhoe-kun <rbornea_230000002480@uic.edu.ph>
2026-09-15 12:43:43 +0000

    Exercise 0 complete

---
+++ devops-automation/hello_automation.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+response = requests.get('htpp://localhost:8000/api/')
+print(f"NetBox Status: {responding.status_code}")
+if response.status_code == 200: 
+    print("NetBox is running successfully!")
+else:
+    print("Failed to connect to NetBox")
